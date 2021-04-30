0ebc80cfb38609aa6a1b9952c51875a2bad09075
Quincy <q.baljeu@gmail.com>
2021-04-30 15:29:03 +0200

    Created DFA class with char export and alphabet check

---
+++ FormeleMethoden/FormeleMethoden/Finite Automatons/DeterministicFiniteAutomaton.cs
@@ -0,0 +1,52 @@
+﻿using FormeleMethoden.States;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FormeleMethoden.Finite_Automatons
+{
+    class DeterministicFiniteAutomaton
+    {
+        private List<Char> _alphabet;
+        private List<DFAState> _states;
+
+        public DeterministicFiniteAutomaton(List<Char> alphabet)
+        {
+            this._alphabet = alphabet;
+        }
+
+        public bool accept(string s)
+        {
+            if (checkAlphabet(s))
+            {
+                foreach (char c in s)
+                {
+                    Console.WriteLine(c);
+                }
+            }
+       
+            return false;
+        }
+
+        public bool checkAlphabet(string s)
+        {
+            foreach(char c in s)
+            {
+                if (!_alphabet.Contains(c))
+                {
+                    Console.WriteLine("Given string contains chars not in DFA's alphabet");
+                    return false;
+                }
+                    
+            }
+
+            return true;
+        }
+
+        public void addState(DFAState state)
+        {
+            _states.Add(state);
+        }
+
+    }
+}
--- FormeleMethoden/FormeleMethoden/Program.cs
+++ FormeleMethoden/FormeleMethoden/Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using FormeleMethoden.Finite_Automatons;
+using System;
+using System.Collections.Generic;
 
 namespace FormeleMethoden
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                List<Char> alphabet = new List<char> { 'a', 'b' };
+
+                DeterministicFiniteAutomaton dfa = new DeterministicFiniteAutomaton(alphabet);
+
+                string s = Console.ReadLine();
+
+                dfa.accept(s);
+            }
         }
     }
 }
